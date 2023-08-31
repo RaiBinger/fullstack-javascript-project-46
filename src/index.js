594695ba0fcce2c279af25ccfa369ab9a590dfa2
Alexander Romanov <raibinger@gmail.com>
2023-08-31 18:04:11 +0300

    refactiring index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import _ from 'lodash';
 
 const getAbsPath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
 const getParsed = (file) => JSON.parse(file);
@@ -11,30 +12,27 @@ const genDiff = (pathToFileOne, pathToFileTwo) => {
   const parsedFileOne = getParsed(fileOneContent);
   const parsedFileTwo = getParsed(fileTwoContent);
 
-  const keysFileOne = Object.keys(parsedFileOne).sort();
-  const keysFileTwo = Object.keys(parsedFileTwo).sort();
-
-  const checkedFileOneByFileTwo = keysFileOne.reduce((acc, keyFileOne) => {
-    let string;
-    if (!parsedFileTwo.hasOwnProperty(keyFileOne)) {
-      string = `  - ${keyFileOne}: ${parsedFileOne[keyFileOne]}`;
-    }
-    if (parsedFileTwo.hasOwnProperty(keyFileOne)) {
-      if (parsedFileTwo[keyFileOne] === parsedFileOne[keyFileOne]) {
-        string = `    ${keyFileOne}: ${parsedFileOne[keyFileOne]}`;
-      } else {
-        string = [`  - ${keyFileOne}: ${parsedFileOne[keyFileOne]}`, `  + ${keyFileOne}: ${parsedFileTwo[keyFileOne]}`];
-      }
-    }
-    return [...acc, string];
-  }, []).flat();
+  const keysFileOne = Object.keys(parsedFileOne);
+  const keysFileTwo = Object.keys(parsedFileTwo);
+  const allKeys = _.uniq(keysFileOne.concat(keysFileTwo).sort());
   
-  const checkedFileTwoByFileOne = keysFileTwo.reduce((acc, keyFileTwo) => {
-    if (!parsedFileOne.hasOwnProperty(keyFileTwo)) {
-      return [...acc, `  + ${keyFileTwo}: ${parsedFileTwo[keyFileTwo]}`];
+  const checkedFileOneByFileTwo = allKeys.flatMap((key) => {
+    let string;
+    if (!parsedFileTwo.hasOwnProperty(key)) {
+      return `  - ${key}: ${parsedFileOne[key]}`;
     }
-    return acc;
-  }, checkedFileOneByFileTwo);
-  return console.log(`{\n${checkedFileTwoByFileOne.join('\n')}\n}`);
+
+    if (!parsedFileOne.hasOwnProperty(key)) {
+      return `  + ${key}: ${parsedFileTwo[key]}`;
+    }
+
+    if (parsedFileTwo[key] === parsedFileOne[key]) {
+      return `    ${key}: ${parsedFileOne[key]}`;
+    }
+
+    return [`  - ${key}: ${parsedFileOne[key]}`, `  + ${key}: ${parsedFileTwo[key]}`];
+  });
+
+  return console.log(`{\n${checkedFileOneByFileTwo.join('\n')}\n}`);
 };
 export default genDiff;
