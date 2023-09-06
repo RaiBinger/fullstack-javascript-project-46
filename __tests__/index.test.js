691d46d95c8e28031d3e24fb32d10c3fd4e4be6f
Alexander Romanov <raibinger@gmail.com>
2023-09-06 21:05:25 +0300

    refactoring tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,6 +9,11 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const expectedContent = fs.readFileSync(getFixturePath('expected_file.txt', 'utf-8'));
 
-test('genDiff', () => {
+test('genDiff JSON', () => {
   expect(genDiff(getFixturePath('./file1.json'), getFixturePath('./file2.json'))).toBe(String(expectedContent));
+  expect(genDiff(getFixturePath('./file1.yaml'), getFixturePath('./file2.yml'))).toBe(String(expectedContent));
+});
+
+test('genDiff YAML', () => {
+  expect(genDiff(getFixturePath('./file1.yaml'), getFixturePath('./file2.yml'))).toBe(String(expectedContent));
 });
